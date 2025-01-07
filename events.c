slimbook/qc71_laptop

commit 9da7bdbdb6615259253ee4e0416efe3bae67069b

events: add placeholder function instead of #if

events.c
@@ -118,6 +118,9 @@ static void emit_keyboard_led_hw_changed(void)
 
 	up_read(&leds_list_lock);
 }
+#else
+static inline emit_keyboard_led_hw_changed(void)
+{ }
 #endif
 
 static void qc71_wmi_event_d2_handler(union acpi_object *obj)
@@ -263,10 +266,7 @@ static void qc71_wmi_event_d2_handler(union acpi_object *obj)
 	/* keyboard backlight brightness changed */
 	case 240:
 		pr_info("keyboard backlight changed\n");
-
-#if IS_ENABLED(CONFIG_LEDS_BRIGHTNESS_HW_CHANGED)
 		emit_keyboard_led_hw_changed();
-#endif
 		break;
 
 	default:

commit ebab4af0b2c5b162bb9f27c80cd284c36b8fb7a9

events: add missing return type
The missing return type causes it to be defaulted to `int`,
which subsequently results in a compilation error if
`-Werror=return-type` or similar is set.

Fixes: 9da7bdbdb661 ("events: add placeholder function instead of #if")

events.c
@@ -120,7 +120,7 @@ static void emit_keyboard_led_hw_changed(void)
 	up_read(&leds_list_lock);
 }
 #else
-static inline emit_keyboard_led_hw_changed(void)
+static inline void emit_keyboard_led_hw_changed(void)
 { }
 #endif
 